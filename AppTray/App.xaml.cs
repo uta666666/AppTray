0d3d1d1549eb50f2cb45a8ed03b85b196850237b
uta666666 <slow-life-sr20@hotmail.co.jp>
2022-12-03 09:51:15 +0900

    多重軌道防止

--- AppTray/App.xaml.cs
+++ AppTray/App.xaml.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
 
-namespace AppTray {
+namespace AppTray
+{
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
-    public partial class App : Application {
+    public partial class App : Application
+    {
+        [System.STAThreadAttribute()]
+        static public void Main()
+        {
+            const string SemaphoreName = "AppTray";
+            bool createdNew;
+
+            // Semaphoreクラスのインスタンスを生成し、アプリケーション終了まで保持する
+            using (var semaphore = new System.Threading.Semaphore(1, 1, SemaphoreName, out createdNew))
+            {
+                if (!createdNew)
+                {
+                    // 他のプロセスが先にセマフォを作っていた
+                    WriteLog("すでに起動しています (AppTray)");
+                    return; // プログラム終了
+                }
+                var app = new App();
+                app.InitializeComponent();
+                app.StartupUri = new Uri("Views/MainWindow.xaml", UriKind.Relative);
+                app.DispatcherUnhandledException += (o, e) => WriteLog(e.Exception);
+                app.Run();
+            }
+        }
+
+        private static void WriteLog(Exception ex)
+        {
+            File.AppendAllText(@".\applog.log", $"[{DateTime.Now.ToString("yyyyMMddHHmmss")}] {ex.ToString()}" + Environment.NewLine);
+        }
+
+        private static void WriteLog(string message)
+        {
+            File.AppendAllText(@".\applog.log", $"[{DateTime.Now.ToString("yyyyMMddHHmmss")}] {message}" + Environment.NewLine);
+        }
     }
 }
